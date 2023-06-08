eef89a30f4bdcdee4f088ee40b49f1af00492691
syyby <2419110160@qq.com>
2023-06-08 15:39:23 +0800

    Update README.md

--- src/main/java/com/example/todolist/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/todolist/service/impl/UserServiceImpl.java
@@ -1,2 +1,24 @@
-package com.example.todolist.service.impl;public class UserServiceImpl {
+package com.example.todolist.service.impl;
+
+import com.example.todolist.dao.UserDao;
+import com.example.todolist.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+    @Autowired
+    private UserDao userDao;
+    @Override
+    public Boolean login(String username, String password) {
+        System.out.println(userDao.getUserInfo(username,password));
+        return true;
+    }
+    public Boolean register(String username,String password, String email)
+    {
+        System.out.println(userDao.addUser(username,password,email));
+        return true;
+    }
+
+
 }
