c8d6f6e6a84fe3e928ad477bf72f6f57a05353a0
syyby <2419110160@qq.com>
2023-06-08 15:39:13 +0800

    Update README.md

--- src/main/java/com/example/todolist/dao/UserDao.java
+++ src/main/java/com/example/todolist/dao/UserDao.java
@@ -1,2 +1,12 @@
-package com.example.todolist.dao;public interface UserDao {
+package com.example.todolist.dao;
+
+import com.example.todolist.pojo.Do.UserInfo;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+@Mapper
+public interface UserDao {
+    UserInfo getUserInfo(@Param("username") String username , @Param("password") String password);
+    UserInfo addUser(@Param("username")String username,@Param("password")String password, @Param("email")String email);
 }
+
