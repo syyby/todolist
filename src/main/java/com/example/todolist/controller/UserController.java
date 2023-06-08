69606fed32978ee701191a7022d8c04a49f8c2e4
syyby <2419110160@qq.com>
2023-06-08 15:39:08 +0800

    Update README.md

--- src/main/java/com/example/todolist/controller/UserController.java
+++ src/main/java/com/example/todolist/controller/UserController.java
@@ -1,2 +1,27 @@
-package com.example.todolist.controller;public class UserController {
+package com.example.todolist.controller;
+
+import com.example.todolist.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+//@Controller
+//@ResponseBody
+public class UserController {
+    @Autowired
+    private UserService userService;
+    @PostMapping("/login")
+    public String login(){
+        userService.login("用户名","密码");
+        return "登陆成功";
+    }
+    @PostMapping("/register")
+    public String register(){
+        userService.register("用户名","密码","邮箱");
+        return "注册成功";
+    }
+
 }
